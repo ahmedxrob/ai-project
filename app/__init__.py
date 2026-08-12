cbd8f2590287e0d304fd39456ae433d0f0048882
ahmedxrob <ahmed.lmryoul@gmail.com>
2026-08-12 13:39:05 +0100

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+ai-project/
+│
+├── app/
+│   ├── __init__.py       ← new
+│   ├── main.py           ← fixed
+│   ├── database.py
+│   │
+│   ├── templates/
+│   │   └── index.html
+│   │
+│   └── static/
+│       └── style.css
+│
+├── Dockerfile
+└── requirements.txt
